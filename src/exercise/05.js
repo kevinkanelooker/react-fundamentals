b827b3f2775006675e3af9eec7153729d1a8a63b
kevinkanelooker <kevin.kane@looker.com>
2020-10-27 12:04:53 -0700

    implement 5 with extra credits

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -12,16 +12,35 @@ import '../box-styles.css'
 
 // 🐨 add a style prop to each of them as well so their background color
 // matches what the text says it should be as well as `fontStyle: 'italic'`
-const smallBox = <div>small lightblue box</div>
-const mediumBox = <div>medium pink box</div>
-const largeBox = <div>large orange box</div>
+const Box = ({style, size, className = '', ...otherProps}) => {
+  const sizeClass = size ? 'box--' + size : ''
+  return (
+    <div
+      className={`box ${sizeClass} ${className}`}
+      style={{fontStyle: 'italic', ...style}}
+      {...otherProps}
+    />
+  )
+}
 
 function App() {
   return (
     <div>
-      {smallBox}
-      {mediumBox}
-      {largeBox}
+      {
+        <Box size="small" style={{backgroundColor: 'lightblue'}}>
+          small lightblue box
+        </Box>
+      }
+      {
+        <Box size="medium" style={{backgroundColor: 'pink'}}>
+          medium pink box
+        </Box>
+      }
+      {
+        <Box size="large" style={{backgroundColor: 'orange'}}>
+          large orange box
+        </Box>
+      }
     </div>
   )
 }
